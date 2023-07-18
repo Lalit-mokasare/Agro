15657ebe57ab50464ad52c3aa0b1e3b9c46566f3
DELL <lmokasare@gmail.com>
2023-07-17 23:27:51 -0700

    This is Main comment

---
+++ src/main/java/com/MahindraTech/agro/AgroApplication.java
@@ -0,0 +1,21 @@
+package com.MahindraTech.agro;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroApplication.class, args);
+
+
+		System.out.println("Hello India");
+
+	}
+
+
+
+
+}
